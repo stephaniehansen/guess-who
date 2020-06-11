0e81d0ec4a595a489477da2b34e3a85b543a5128
Stephanie Hansen <stephanie@stephanie.dev>
2020-06-11 19:31:00 +0100

    add final features and code refactoring

--- main.js
+++ main.js
@@ -3,98 +3,114 @@ import questions from "./data/questions.js";
 
 const selected = document.getElementById("questions");
 const board = document.getElementById("grid");
+const questionHTML = document.querySelector(".computer .question");
+const buttons = document.querySelectorAll(".computer .btn");
+
 let toBeAsked = [...questions];
+let computersBoard = [...characters];
+let playersBoard = [...characters];
 let playersCard = "";
 let computersCard = "";
 
-const removeCards = (filteredCards) => {
-    const cards = document.querySelectorAll(".card");
+const endCondition = () => {
+    return playersBoard.length === 1 || computersBoard.length === 1;
+}
 
-    filteredCards.forEach(match => {
+const endGame = () => {    
+    alert(`Game Over. Player has ${playersBoard.length} remaining cards and Computer has ${computersBoard.length}`);
+}
+
+const eliminate = (character) => {
+    const cards = document.querySelectorAll(".card");
         cards.forEach(card => {
-            if(card.textContent === match.name){
+            if(card.textContent === character.name) {
                 card.classList.add("eliminated");
             }
         })
-    })
 }
 
-const filterCards = (selectedQuestion) => {
+const updateBoard = (player, selectedQuestion, cardToCheck, gameBoard) => {
     let key = "";
     let value = "";
 
     questions.forEach(question => {
-        if(question.id === selectedQuestion){
+        if(question.id === selectedQuestion) {
             key = question.match[0];
             value = question.match[1];
         }
     })
 
-    const filteredCards = characters.filter(character => {
-        if (computersCard[key] === value) {
-            return character[key] !== value;
-        } else if (computersCard[key] !== value) {         
-            return character[key] === value; 
+    return gameBoard.filter(character => {
+        if (cardToCheck[key] === value) {
+            if(character[key] !== value && player === true) {
+                eliminate(character);
+            } return character[key] === value;
+        } else if (cardToCheck[key] !== value) {
+            if(character[key] === value && player === true) {
+                eliminate(character);
+            } return character[key] !== value; 
         }
     })
-    removeCards(filteredCards);
 }
 
-const computerSelectQuestion = () => {
-    const questionHTML = document.querySelector(".computers-questions .question");
-    const buttons = document.querySelectorAll(".computers-questions button");
+const computersTurn = () => {
     const random = Math.floor(Math.random()*toBeAsked.length);
-    let selectedQuestion = toBeAsked[random];
-
-    questionHTML.innerHTML = selectedQuestion.question;
+    const generated = toBeAsked[random];
+    questionHTML.innerHTML = generated.question;
     toBeAsked.splice(random, 1);
 
     buttons.forEach(button => {
         button.addEventListener("click", (event) => {
+            event.preventDefault();
             selected.disabled = false;
-            playerSelectQuestion();
+            computersBoard = updateBoard(false, generated.id, playersCard, computersBoard);
+
+            if(endCondition()) {
+                endGame();
+            }
         });
     });
 }
 
-const playerSelectQuestion = () => {
-    selected.addEventListener("change", (event) => {
-        const selectedQuestion = selected.value;
-        filterCards(selectedQuestion);
-        selected.remove(selected.selectedIndex);
-        selected.disabled = true;
-        computerSelectQuestion();
-    });
-}
-
-const generateQuestions = () => {
+const populateQuestions = () => {
     questions.forEach(option => {
         document.getElementById("questions").innerHTML += `<option value="${option.id}">${option.question}</option>`;
     })
 }
 
-const generateCardSelection = () => {
+const drawComputersCard = () => {
     return characters[Math.floor(Math.random()*characters.length)];
-}
-
-const startGame = () => {
-    computersCard = generateCardSelection();
-    generateQuestions();
-    playerSelectQuestion();
 }
 
 const setup = () => {
     characters.forEach(character => {
         board.innerHTML += `<div class="card">${character.name}</div>`;
     })
-
-    const cards = document.querySelectorAll(".card");
-    cards.forEach(card => {
-        card.addEventListener("click", (event) => {
-            playersCard = card.textContent;
-            startGame();
-        })
-    });
 }
+
+document.addEventListener("click", (event) => {
+    if(event.target.matches(".card")) {
+        event.preventDefault();
+        playersCard = characters.filter(character => {
+            return event.target.textContent === character.name})[0];
+        computersCard = drawComputersCard();
+        populateQuestions();
+    }
+}, {once:true});
+
+document.addEventListener("change", (event) => {
+    if(event.target.matches("#questions")){
+        const selectedQuestion = selected.value;
+        playersBoard = updateBoard(true, selectedQuestion, computersCard, playersBoard);
+        selected.remove(selected.selectedIndex);
+        selected.disabled = true;
+        
+        if(endCondition()) {
+            endGame();
+        } else {
+            computersTurn();
+        }
+    }
+});
 
 setup();
